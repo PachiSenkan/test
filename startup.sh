b9636fc8ac9e0787588ca3494b957ea69d368766
DaPassan <ya.jeans2012@yandex.ru>
2024-09-26 00:58:57 +0700

    Fixed docker startup

--- startup.sh
+++ startup.sh
@@ -7,10 +7,9 @@ while !</dev/tcp/db/5432;
     do sleep 1;
 done;
 
-# Run migrations
+mkdir -p app/alembic/versions
+
 alembic revision --autogenerate
 alembic upgrade head
 
 uvicorn app.main:app --host 0.0.0.0
-# Create initial data in DB
-#python app/initial_data.py
